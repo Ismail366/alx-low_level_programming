2b3e280a4b58938488e191892a38ca2e208339b0
Ismail Adamu <adamuismail165@gmail.com>
2022-09-08 10:35:35 -0700

    Added 5-printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: prints exactly
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a newline
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
