4d795a38c006ae3a6b980f6ea081e27a9d8ddbad
Ismail Adamu <adamuismail165@gmail.com>
2022-09-08 14:43:06 -0700

    Fixed missing )

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("The size of an int: %lu byte(s)\n", sizeof(int));
 	printf("The size of long int: %lu byte(s)\n", sizeof(long int));
 	printf("The size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("The size of a float: %lu byte(s)\n", sizeof(float);
+	printf("The size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
